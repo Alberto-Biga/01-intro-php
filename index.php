eb3dd78caa639aac6a3259fce7c91d79f145cfa5
AlbertoBiga <alberto.biga10@gmail.com>
2022-05-04 13:36:20 +0200

    modifiche

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php 
-
-
 //variabili
 $nome = 'Alberto';
 $nome2 = 'Michele';
 //echo stampa direttamente a schermo (elemento non 'trovato' attraverso l'inspector)
 echo $nome;
-
-
 //array
 $array= ['banana', 'mela', 'pera', 'albicocca'];
-// var_dump($array);
+//http://localhost/php-boolean/01-intro-php/?nome=luigi 
+//con $GEt riesco a recuperare e salvare il nome che io scrivo (in questo caso luigi)
+$parametroUri = ( isset($_GET['nome']) ) ? $_GET['nome'] : '' ; //$_GET['nome']
 
+//creo costante per salvare il contenuto del form attraverso get
+$parametro2 = $_GET['parametro2'];
+
+$paragrafo = '           lorem ipsum dolor sit amet, consectetur adipisicing elit.            '   ;
+$paragrafoDopoTrim = trim($paragrafo)
 ?>
 
 
@@ -29,23 +32,63 @@ $array= ['banana', 'mela', 'pera', 'albicocca'];
     <h3>
         Ciao <?php echo $nome; ?>
     </h3>
-
     <h3>
         Ciao <?php echo $nome2; ?>
     </h3>
-
     <p>
         Ciao <?php echo $nome.'. Io sono '.$nome2 ?>
     </p>
-
     <!-- non mi serve, vedo gia' array nella maniera piu' comoda possibile. Nel caso non avessi xdebug dovrei scrivere questo codice
     <?//php echo '<pre>' ?>
     <?//php var_dump($array); ?>
     <?//php echo '<pre>' ?> -->
-
     <p>
         <?php  echo var_dump($array); ?>
     </p>
+    <span>
+        <?php echo $parametroUri ?>
+    </span>
+    <!-- con il form riesco ad inviare e poi salvare un nuovo dato che verra' automaticamente salvato a schermo -->
+    <form method="GET">
+        <input type="text" name="parametro2">
+        <input type="submit">
+    </form>
+    <?php echo $parametro2 ?>
 
+    <!-- EXPLODE -->
+    <!-- in base al delimitatore, in questo caso il -  , divide la stringa data ogni volta che trova il delimitatore-->
+    <?php var_dump( explode( '-', 'Alberto-Biga-10/11/1998-Torino') ) ?>
+
+    <!-- TRIM (vedi riga 17) -->
+    <!-- toglie spazi vuoti -->
+    <?php echo strlen($paragrafo); ?>
+    <!-- STRLEN -->
+    <!-- misura lunghezza stringa -->
+    <?php echo strlen($paragrafoDopoTrim) ?>
+
+    <!-- REPLACE -->
+    <!-- rimpizza una parola con un'altra -->
+    <p>
+        <?php echo str_replace('amet', '####', $paragrafo)?>
+    </p>
+    
+    <!-- STRPOS -->
+    <!-- individuo la prima posizione di una parola -->
+    <p>
+        Ho trovato la parola nella posizione (indico solo la prima posizione) :
+        <?php echo strpos($paragrafo, 'sit') ?>
+    </p>
+
+    <!-- UCFIRST -->
+    <!-- prima parola maiuscola, solo se senza spazi davanti -->
+    <p>
+        <?php echo ucfirst($paragrafoDopoTrim);?>
+    </p>
+
+    <!-- UCWORDS -->
+    <!-- rende maiuscole tutte le parole -->
+    <p>
+        <?php echo ucwords($paragrafoDopoTrim);?>
+    </p>
 </body>
 </html>
